bf29ec2acfdc84c047c92797790d8b03a853dda8
tushar0392 <80540021+tushar0392@users.noreply.github.com>
2022-03-02 14:33:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-
+app.get("/",(req,res)=>{
+    res.send("test");
+})
 app.post("/bfhl", (req, res) => {
     const {data} = req.body;
     const numbers = [];
